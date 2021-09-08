59d2b8365bf86153bc0790deec101842550cda0d
cola0831 <lsh20050722@gmail.com>
2021-09-08 15:30:22 +0900

    0.0.1
    sacsdfefwf

---
+++ Munsan-game-1/Assets/Scripts/Door_Key.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Key : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float RandomKey_1=0;
+    public float RandomKey_2=0;
+    public float RandomKey_3=0;
+    public float RandomKey_4=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameManager = GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameManager.RandomKey.gameObject.SetActive(true);
+            gameManager.RandomKeyPaper = true;
+            
+        }
+    }
+}
